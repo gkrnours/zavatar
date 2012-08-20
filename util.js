7ed3f2f20e1992a001fa14899070a55cca0d574c
Coues Ludovic <gkr@jellycopter.net>
2012-08-20 19:24:38 +0200

    cleaned util.js

--- util.js
+++ util.js
@@ -10,33 +10,6 @@ this.process = function(flux){
 	var payload = []
 	var l=fmap.zone.length
 
-	for(i=0; i<l; ++i){
-		var local = fmap.zone[i]["@"]
-		var building = fmap.zone[i].building
-		if(local.nvt == 0){
-			payload.push(id+":"+local.x+"_"+local.y+":last")
-			payload.push(flux.headers.game["@"].days)
-		}
-		payload.push(id+":"+local.x+"_"+local.y+":tag")
-		payload.push(local.tag)
-		payload.push(id+":"+local.x+"_"+local.y+":danger")
-		payload.push(local.danger)
-		payload.push(id+":"+local.x+"_"+local.y+":type")
-		if(local.x == cpos[0] && local.y == cpos[1])
-			payload.push("c")
-		else if(typeof(building) == "undefined")
-			payload.push((local.nvt == 0)?"t":"v")
-		else
-			payload.push("x")
-	}
-	payload.push(id+":city:x")
-	payload.push(cpos[0])
-	payload.push(id+":city:y")
-	payload.push(cpos[1])
-	payload.push(id+":city:w")
-	payload.push(fmap["@"].wid)
-	payload.push(id+":city:h")
-	payload.push(fmap["@"].hei)
 	payload.push(id+":city:name")
 	payload.push(city.city)
 	return payload
@@ -49,12 +22,6 @@ this.mk_tpl_val = function mk_tpl_val(req){
 	if(req && req.session) {
 		if(req.session.me)
 			tpl_val.me = req.session.me
-		else
-			tpl_val.me = {name: "Invité"}
-		if(req.session.city)
-			tpl_val.city = req.session.city
-		else
-			tpl_val.city = {name: "Ancienne Cité Oubliée"}
 	}
 	if(req && req.query){
 		if(req.query.e)
@@ -63,6 +30,3 @@ this.mk_tpl_val = function mk_tpl_val(req){
 	return tpl_val
 }
 
-this.isAdmin = function(req, res, next){
-	return next()
-}
