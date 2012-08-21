9eba708e2e80719d5d7054653bdc81ff631419ba
Coues Ludovic <gkr@jellycopter.net>
2012-08-21 22:26:02 +0200

    fix a bug with iinput being string and not number

--- bin.js
+++ bin.js
@@ -2,6 +2,7 @@ function read(mask, bit){
 	return 1 == Math.floor(mask/Math.pow(2, bit))%2
 }
 function set(mask, bit){
+	mask = parseInt(mask,10)
 	if(typeof(bit) == "object"){
 		for(i=0;i<bit.length;++i){
 			mask = set(mask, bit[i])
@@ -14,6 +15,7 @@ function set(mask, bit){
 		return mask + Math.pow(2,bit)
 }
 function unset(mask, bit){
+	mask = parseInt(mask,10)
 	if(typeof(bit) == "object"){
 		for(i=0;i<bit.length;++i){
 			mask = unset(mask, bit[i])
